b696a0a7e3a54241976f688b2915f2875bf0f1df
efeckgz <efeckgz@gmail.com>
2024-12-01 20:35:45 +0300

    Changed to a black background.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,52 @@
 "use client";
 
+import { useState } from "react";
+
 interface MenuButtonProps {
   title: string;
   action: () => void;
 }
 
-const buttons: MenuButtonProps[] = [
-  {
-    title: "Create new vault",
-    action: () => {
-      console.log("Created new vault!");
-    },
-  },
-  {
-    title: "Open a vault",
-    action: () => {
-      console.log("Opened a vault!");
-    },
-  },
-  {
-    title: "Delete a vault",
-    action: () => {
-      console.log("Deleted a vault!");
-    },
-  },
-];
-
 export default function Home() {
+  const [vaultsShown, setVaultsShown] = useState(false);
+
+  const buttons: MenuButtonProps[] = [
+    {
+      title: "Create new vault",
+      action: () => {
+        console.log("Created new vault!");
+      },
+    },
+    {
+      title: "Open a vault",
+      action: () => {
+        console.log("Opened a vault!");
+        setVaultsShown(true);
+      },
+    },
+    {
+      title: "Delete a vault",
+      action: () => {
+        console.log("Deleted a vault!");
+      },
+    },
+  ];
+
   return (
     <div className="flex flex-col items-center justify-between h-screen py-20">
       <div className="flex flex-col items-center">
-        <h1 className="text-6xl text-indigo-900 font-bold">
-          Welcome to SecureMe.
-        </h1>
-        <p className="text-black">Please start by selecting an option below.</p>
+        <h1 className="text-6xl text-white font-bold">Welcome to SecureMe.</h1>
+        <p className="text-white">Please start by selecting an option below.</p>
       </div>
       <div className="flex flex-col space-y-2">
         {buttons.map(({ title, action }, index) => {
           return (
             <button
-              className="border-0 rounded-lg bg-black/20"
+              className="border-0 rounded-lg bg-white/20"
               onClick={action}
               key={index}
             >
-              <div className="my-2 mx-12 text-black">{title}</div>
+              <div className="my-2 mx-12 text-white">{title}</div>
             </button>
           );
         })}
