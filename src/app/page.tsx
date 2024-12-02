c24490a9725598cebd8978dfd32d4c8610113f0c
efeckgz <efeckgz@gmail.com>
2024-12-02 10:40:01 +0300

    Worked on modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,35 +33,39 @@ export default function Home() {
   ];
 
   return (
-    <>
-      <div className="border border-lg w-600 h-400">Hello</div>
-      <div
-        className={`flex flex-col items-center justify-between h-screen py-20 ${
-          vaultsShown ? "bg-white/10" : ""
-        }`}
-      >
-        <div className="flex flex-col items-center">
-          <h1 className="text-6xl text-white font-bold">
-            Welcome to SecureMe.
-          </h1>
-          <p className="text-white">
-            Please start by selecting an option below.
-          </p>
-        </div>
-        <div className="flex flex-col space-y-2">
-          {buttons.map(({ title, action }, index) => {
-            return (
-              <button
-                className="border-0 rounded-lg bg-white/20"
-                onClick={action}
-                key={index}
-              >
-                <div className="my-2 mx-12 text-white">{title}</div>
-              </button>
-            );
-          })}
-        </div>
-      </div>
-    </>
+    <div className="flex justify-center items-center text-black">
+      <VaultsModal>Hello</VaultsModal>
+    </div>
+    // <div
+    //   className={`flex flex-col items-center justify-between h-screen py-20 ${
+    //     vaultsShown ? "bg-white/10" : ""
+    //   }`}
+    // >
+    //   <div className="flex flex-col items-center">
+    //     <h1 className="text-6xl text-white font-bold">Welcome to SecureMe.</h1>
+    //     <p className="text-white">Please start by selecting an option below.</p>
+    //   </div>
+    //   <div className="flex flex-col space-y-2">
+    //     {buttons.map(({ title, action }, index) => {
+    //       return (
+    //         <button
+    //           className="border-0 rounded-lg bg-white/20"
+    //           onClick={action}
+    //           key={index}
+    //         >
+    //           <div className="my-2 mx-12 text-white">{title}</div>
+    //         </button>
+    //       );
+    //     })}
+    //   </div>
+    // </div>
   );
 }
+
+const VaultsModal = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="border-8 border-white w-[700px] h-[400px] bg-white">
+      {children}
+    </div>
+  );
+};
