3e14d90e8fc05e7822af692f96b004f6ddd14660
efeckgz <efeckgz@gmail.com>
2024-12-02 22:07:40 +0300

    Modal work. Added svg for image.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { useState } from "react";
 
 interface MenuButtonProps {
@@ -33,39 +34,54 @@ export default function Home() {
   ];
 
   return (
-    <div className="flex justify-center items-center text-black">
-      <VaultsModal>Hello</VaultsModal>
+    <div className="relative h-screen">
+      {/* Modal */}
+      {vaultsShown && (
+        <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center z-10">
+          <div className="relative border-0 w-[700px] h-[500px] bg-white">
+            <button
+              className="absolute top-4 left-4 text-white text-2xl font-bold px-4 py-2"
+              onClick={() => setVaultsShown(false)}
+            >
+              <Image
+                src="/svgviewer-png-output.png"
+                alt="close"
+                width={32}
+                height={32}
+              />
+            </button>
+          </div>
+        </div>
+      )}
+
+      {/* Main View */}
+      <div
+        className={`flex flex-col items-center justify-between h-screen py-20 ${
+          vaultsShown ? "bg-white/10" : ""
+        } z-0`}
+      >
+        <div className="flex flex-col items-center">
+          <h1 className="text-6xl text-white font-bold">
+            Welcome to SecureMe.
+          </h1>
+          <p className="text-white">
+            Please start by selecting an option below.
+          </p>
+        </div>
+        <div className="flex flex-col space-y-2">
+          {buttons.map(({ title, action }, index) => {
+            return (
+              <button
+                className="border-0 rounded-lg bg-white/20"
+                onClick={action}
+                key={index}
+              >
+                <div className="my-2 mx-12 text-white">{title}</div>
+              </button>
+            );
+          })}
+        </div>
+      </div>
     </div>
-    // <div
-    //   className={`flex flex-col items-center justify-between h-screen py-20 ${
-    //     vaultsShown ? "bg-white/10" : ""
-    //   }`}
-    // >
-    //   <div className="flex flex-col items-center">
-    //     <h1 className="text-6xl text-white font-bold">Welcome to SecureMe.</h1>
-    //     <p className="text-white">Please start by selecting an option below.</p>
-    //   </div>
-    //   <div className="flex flex-col space-y-2">
-    //     {buttons.map(({ title, action }, index) => {
-    //       return (
-    //         <button
-    //           className="border-0 rounded-lg bg-white/20"
-    //           onClick={action}
-    //           key={index}
-    //         >
-    //           <div className="my-2 mx-12 text-white">{title}</div>
-    //         </button>
-    //       );
-    //     })}
-    //   </div>
-    // </div>
   );
 }
-
-const VaultsModal = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="border-8 border-white w-[700px] h-[400px] bg-white">
-      {children}
-    </div>
-  );
-};
