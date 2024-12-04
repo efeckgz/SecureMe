a834bbb0eef7020cf5fc696afe70f2a656d06c85
efeckgz <efeckgz@gmail.com>
2024-12-04 17:44:08 +0300

    Basic password hashing implementation.

--- src-tauri/src/vault.rs
+++ src-tauri/src/vault.rs
@@ -5,4 +5,5 @@ pub fn create_secure_vault(path: String, password: String) {
     // Interpret all files as byte arrays and encrypt them with the key.
     // concatanate all the cipeher-byte arrays into one cipher file - the vault
     println!("Created vault: {}, {}.", path, password);
+    println!("Password hash: {}", sha256::digest(password));
 }
