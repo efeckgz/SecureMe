ef526c13ba6b9531535fbea11d3a91b31aeaa6fb
efeckgz <efeckgz@gmail.com>
2024-12-07 17:27:03 +0300

    More smodal styling

--- src/app/components/selectVaults.tsx
+++ src/app/components/selectVaults.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { invoke } from "@tauri-apps/api/core";
-import { X, Trash2 } from "@geist-ui/icons";
+import { X, Trash2, Lock, Unlock } from "@geist-ui/icons";
 
 const SelectVaults = ({ closeFunc }: { closeFunc: () => void }) => {
   const [vaults, setVaults] = useState([]);
@@ -35,9 +35,9 @@ const SelectVaults = ({ closeFunc }: { closeFunc: () => void }) => {
             <span>Last accessed</span>
           </div>
         </div>
-
+        {/* h-[calc(100%-4rem)] */}
         {/* Modal Content */}
-        <div className="overflow-y-auto h-[calc(100%-4rem)]">
+        <div className="overflow-y-auto h-[calc(97%-4rem)]">
           {/* <p className="text-center text-gray-400">Your vault content...</p> */}
           {vaults.map(({ name, path, isLocked }: VaultViewModel, key) => {
             return (
@@ -45,7 +45,23 @@ const SelectVaults = ({ closeFunc }: { closeFunc: () => void }) => {
                 className="flex justify-between items-center w-full px-4 py-4 border-b border-white/10 "
                 key={key}
               >
-                {name}
+                <div className="flex flex-row items-center space-x-4">
+                  <button className="p-2 rounded hover:bg-white/10">
+                    <Trash2 />
+                  </button>
+                  <div className="flex flex-col">
+                    <h1 className="font-bold text-lg">{name}</h1>
+                    <h2 className="font-thin text-sm text-white/50">{path}</h2>
+                  </div>
+                  {/* <Lock /> */}
+                </div>
+                <Lock />
+                <h2 className="font-thin text-sm text-white/50">
+                  Creation date
+                </h2>
+                <h2 className="font-thin text-sm text-white/50">
+                  Last accessed
+                </h2>
               </div>
             );
           })}
